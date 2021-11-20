1689940a41aade058aa9a7b36b51e2241570bbc9
sviridov <sviridov.dev@gmail.com>
2021-11-20 18:18:38 +0300

    db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => {
+                return {
+                    type: 'postgres',
+                    host: configService.get('POSTGRES_HOST'),
+                    port: configService.get('POSTGRES_PORT'),
+                    username: configService.get('POSTGRES_USERNAME'),
+                    password: configService.get('POSTGRES_PASSWORD'),
+                    database: 'postgres',
+                    autoLoadEntities: true,
+                    synchronize: true,
+                };
+            },
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
